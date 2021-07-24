0703da567cb0bc18e59c5384afde17759ade8d66
Eswar Rao <eswarraokdipelly@gmail.com>
2021-07-24 17:23:37 +0530

    Update 1

---
+++ src/component/AllWallets.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import './Wallets.css';
+import { arr } from './Data';
+function AllWallets() {
+  return (
+    <>
+      <table className="theTable" align="center" width="60%">
+        <thead>
+          <tr>
+            <th>User Id</th>
+            <th>Name</th>
+            <th>Phone</th>
+            <th>Balance (Rs)</th>
+          </tr>
+        </thead>
+        <tbody>
+          {arr.map((e, idx) => {
+            return (
+              <tr key={idx}>
+                {e.map((ele, index) => {
+                  return <td key={index}>{ele}</td>;
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
+  );
+}
+
+export default AllWallets;
