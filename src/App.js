5e63f362f788f3fa27a7ac7bbbb87f3bd13fedf9
Eswar Rao <eswarraokdipelly@gmail.com>
2021-07-24 17:32:51 +0530

    Update 2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import './App.css';
 import React, { useState } from 'react';
+// import AllWallets from './component/AllWallets';
+// import NewWallet from './component/NewWallet';
+// import CheckBalance from './component/CheckBalance';
+// import AddFunds from './component/AddFunds';
+// import SpendFunds from './component/SpendFunds';
+// import AllTransactions from './component/AllTransactions';
 import AllWallets from './component/AllWallets';
 import NewWallet from './component/NewWallet';
 import CheckBalance from './component/CheckBalance';
